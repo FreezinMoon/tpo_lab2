b981035944f1281608579804a3f315ec233978b2
Petr <petr.gumennik@gmail.com>
2025-04-04 17:20:18 +0300

    log2

--- src/test/java/unit/Log2ModuleRealTest.java
+++ src/test/java/unit/Log2ModuleRealTest.java
@@ -3,38 +3,38 @@ package test.java.unit;
 import main.java.modules.log.LnModule;
 import main.java.modules.log.real.LnModuleReal;
 import main.java.modules.log.real.Log2ModuleReal;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class Log2ModuleRealTest {
+    private Log2ModuleReal log2;
+
+    @Before
+    public void setUp(){
+        LnModule ln = new LnModuleReal(1e-7);
+        log2 = new Log2ModuleReal(ln);
+    }
 
     @Test(expected = ArithmeticException.class)
     public void testLog2NegativeThrows() {
-        LnModule ln = new LnModuleReal(1e-7);
-        Log2ModuleReal log2 = new Log2ModuleReal(ln);
         log2.log2(-1.0);
     }
 
     @Test
     public void testLog2Of1() {
-        LnModule ln = new LnModuleReal(1e-7);
-        Log2ModuleReal log2 = new Log2ModuleReal(ln);
         assertEquals(0.0, log2.log2(1.0), 1e-9);
     }
 
     @Test
     public void testLog2Of2() {
-        LnModule ln = new LnModuleReal(1e-7);
-        Log2ModuleReal log2 = new Log2ModuleReal(ln);
         double actual = log2.log2(2.0);
         assertEquals(1.0, actual, 1e-5);
     }
 
     @Test
     public void testLog2Arbitrary() {
-        LnModule ln = new LnModuleReal(1e-7);
-        Log2ModuleReal log2 = new Log2ModuleReal(ln);
         double x = 10.0;
         double actual = log2.log2(x);
         double expected = Math.log(10.0) / Math.log(2.0);
